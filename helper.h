5226b83f91241473515d9773e5be70a2a2a7721a
mafagafogigante <mafagafogigante@gmail.com>
2014-10-13 12:47:49 -0300

    Reformatted helper.h

--- helper.h
+++ helper.h
@@ -5,26 +5,32 @@
 #include <sstream>
 #include <sys/stat.h>
 
-namespace helper {
+namespace helper
+{
 
-    void print_usage(char *pname) {
-        std::cout << "\nUsage: " << pname << " <file>\n";
-    }
+void print_usage(char *pname)
+{
+    std::cout << "\nUsage: " << pname << " <file>\n";
+}
 
-    // Returns (in bytes) the size of a file.
-    unsigned int get_file_size(const std::string &fname) {
-        struct stat stat_buffer;
-        int rc = stat(fname.c_str(), &stat_buffer);
-        return rc == 0 ? stat_buffer.st_size : -1;
-    }
+// Returns (in bytes) the size of a file.
+int get_file_size(const std::string &fname)
+{
+    struct stat stat_buffer;
+    int rc = stat(fname.c_str(), &stat_buffer);
+    return rc == 0 ? stat_buffer.st_size : -1;
+}
 
-    std::string to_string(unsigned long value) {
-        // The length of 2 ^ 64 - 1 is 20.
-        char buffer[20];
-        // "lu" is used for unsigned long.
-        std::sprintf(buffer, "%lu", value);
-        return buffer;
-    }
+std::string to_string(unsigned long value)
+{
+    // The length of 2 ^ 64 - 1 is 20.
+    // ULONG is usually 2 ^ 32 - 1, but the code sticks to 2 ^ 64 - 1 just in case a implementation eventually
+    // has a 64-bit unsigned long.
+    char buffer[20];
+    // sprintf stores a formatted string into a C-string.
+    std::sprintf(buffer, "%lu", value); // "lu" is used for unsigned long.
+    return buffer;
+}
 }
 
 #endif
